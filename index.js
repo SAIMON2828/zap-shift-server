b61cc87305258568ac08a8e4d829f64ac65424d3
saimon2828 <mdsaimon28285@gmail.com>
2026-06-06 17:31:16 +0600

    deletePost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000
 
@@ -33,13 +33,33 @@ async function run() {
 
         // parcel API
         app.get('/parcels', async (req, res) => {
+            const query = {}
+            const { email } = req.query;
+            // parcels?email = '' &
+            if(email){
+                query.senderEmail = email;
+            }
 
+            const options  = {sort: { createdAt: -1 }}
+            const cursor = parcelsCollection.find(query, options);
+            const result = await cursor.toArray();
+            res.send(result)
         })
 
         app.post('/parcels', async (req, res) => {
             const parcel = req.body;
+            // parcels created time
+            parcel.createdAt = new Date();
+
             const result = await parcelsCollection.insertOne(parcel);
             res.send(result)
+        })
+
+        app.delete('/parcels/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await parcelsCollection.deleteOne(query);
+            res.send(result);
         })
 
 
