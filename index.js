87449c0db7c4efdf95d8eac256247657d999a19f
saimon2828 <mdsaimon28285@gmail.com>
2026-06-23 22:40:25 +0600

    rider

--- index.js
+++ index.js
@@ -5,10 +5,60 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000
+const crypto = require("crypto");
+
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+const serviceAccount = require("./zap-shift-1499b-firebase-adminsdk-fbsvc-b9c46508ac.json");
+
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+
+
+function generateTrackingId() {
+    const prefix = 'PRCL';
+    const date = new Date().toISOString().slice(0, 10).replace(/-/g, ""); //YYYYMMDD
+
+    const randomPart = crypto
+        .randomBytes(4)
+        .toString("base64")
+        .replace(/[^A-Z0-9]/gi, "")
+        .substring(0, 6)
+        .toUpperCase();
+
+    return `${prefix}-${date}-${randomPart}`;
+}
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const varifyFBToken = async (req, res, next) => {
+    // console.log('header in the middleware', req.headers.authorization)
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+
+    try {
+        const idToken = token.split(' ')[1];
+        const decoded = await getAuth().verifyIdToken(idToken);
+        // console.log('Decoded in the token', decoded);
+        req.decoded_email = decoded.email;
+
+        next();
+    }
+    catch (err) {
+       return res.status(401).send( { message: 'unauthorised access'} )
+    }
+
+  
+
+}
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
@@ -32,6 +82,26 @@ async function run() {
 
         const db = client.db('zap_shift_db');
         const parcelsCollection = db.collection('parcels');
+        const paymentCollection = db.collection('payments');
+        const userCollection = db.collection('users');
+        const riderCollection = db.collection('riders');
+
+        // user related api
+
+        app.post('/users', async(req, res)=>{
+               const user = req.body;
+               user.role = 'user';
+               const email = user.email;
+               user.createdAt = new Date();
+
+               const userExist = await userCollection.findOne({ email})
+               if(userExist){
+                return res.send({ message: 'user exist'})
+               }
+
+               const result = await userCollection.insertOne(user);
+               res.send(result);
+        })
 
         // parcel API
         app.get('/parcels', async (req, res) => {
@@ -96,7 +166,7 @@ async function run() {
                 success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
                 cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
             })
-            res.send({ url: session.url})
+            res.send({ url: session.url })
         })
         // payment related API
         app.post('/create-checkout-session', async (req, res) => {
@@ -118,6 +188,7 @@ async function run() {
                 mode: 'payment',
                 metadata: {
                     parcelId: paymentInfo.parcelId,
+                    parcelName: paymentInfo.parcelName,
                 },
                 customer_email: paymentInfo.senderEmail,
                 success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
@@ -128,23 +199,98 @@ async function run() {
             res.send({ url: session.url })
         })
         // checking and update API
-        app.patch('/payment-success', async(req, res)=>{
+        app.patch('/payment-success', async (req, res) => {
             const sessionId = req.query.session_id;
             const session = await stripe.checkout.sessions.retrieve(sessionId);
-            console.log('session retrieve', session);
-            if(session.payment_status === 'paid'){
+            // console.log('session retrieve', session);
+            const transactionId = session.payment_intent;
+            const query = { transactionId: transactionId }
+
+            const paymentExist = await paymentCollection.findOne(query);
+
+            if (paymentExist) {
+                return res.send({
+                    message: 'already exists',
+                    transactionId,
+                    trackingId: paymentExist.trackingId
+                })
+            }
+            const trackingId = generateTrackingId();
+            if (session.payment_status === 'paid') {
                 const id = session.metadata.parcelId;
-                const query = { _id: new ObjectId(id)}
+                const query = { _id: new ObjectId(id) }
                 const update = {
-                    $set:{
+                    $set: {
                         paymentStatus: 'paid',
+                        trackingId: trackingId
                     }
                 }
-                const result = await parcelsCollection.updateOne(query,update);
-                res.send(result);
+                const result = await parcelsCollection.updateOne(query, update);
+
+                const payment = {
+                    amount: session.amount_total / 100,
+                    currency: session.currency,
+                    customerEmail: session.customer_email,
+                    parcelId: session.metadata.parcelId,
+                    parcelName: session.metadata.parcelName,
+                    transactionId: session.payment_intent,
+                    paymentStatus: session.payment_status,
+                    paidAt: new Date(),
+                    trackingId: trackingId
+
+                }
+
+                if (session.payment_status === 'paid') {
+                    const resultPayment = await paymentCollection.insertOne(payment);
+                    res.send({
+                        success: true,
+                        modifyParcel: result,
+                        trackingId: trackingId,
+                        transactionId: session.payment_intent,
+                        paymentInfo: resultPayment
+                    })
+                }
             }
 
-            res.send({success: false})
+            res.send({ success: false })
+        })
+
+        // payment related API
+        app.get('/payments', varifyFBToken, async (req, res) => {
+            const email = req.query.email;
+            const query = {}
+
+            console.log('headers', req.headers);
+            if (email) {
+                query.customerEmail = email;
+
+                // check email adress
+                if(email !== req.decoded_email){
+                    return res.status(403).send({message: 'forbidden access'})
+                }
+            }
+            const cursor = paymentCollection.find(query).sort({paidAt: -1});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // rider related  API
+        app.get('/riders', async(req, res)=>{
+            const query = { }
+            if(req.query.status){
+                query.status = req.query.status;
+            }
+            const curson = riderCollection.find(query);
+            const result =await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/riders', async(req, res) =>{
+            const rider = req.body;
+            rider.status = 'pending';
+            rider.createdAt = new Date();
+            const result = await riderCollection.insertOne(rider);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
